beb90c48438ab99fa2e6c0e6f0125d5c4ec7e755
BOOBATHI THILLAN <128199038+BOOBATHITHILLAN@users.noreply.github.com>
2023-07-15 09:08:37 +0530

    Update HandleApi.js

--- src/api/HandleApi.js
+++ src/api/HandleApi.js
@@ -1,43 +1,53 @@
 import axios from 'axios'
 
-const Url="https://todo-be-ldmc.onrender.com";
+const Url = "https://todo-be-ldmc.onrender.com";
 
-const getAllTodo=(setTodo)=>{
+const getAllTodo = (setTodo) => {
     axios.get(Url)
-    .then(({data})=>{
-        console.log(data);
-        setTodo(data);
-    })
+        .then(({ data }) => {
+            console.log(data);
+            setTodo(data);
+        })
 }
 
-const addTodo=(text,setText,setTodo)=>{
-
-    axios.post(`${Url}/save`,{text})
-    .then(data=>{        
-        setText("")
-        getAllTodo(setTodo)
-    })
-    .catch(err=>console.error(err));
+const addTodo = (text, setText, setTodo, setCheck) => {
+    if (text) {
+        setCheck(false)
+        axios.post(`${Url}/save`, { text })
+            .then(data => {
+                setText("")
+                getAllTodo(setTodo)
+            })
+            .catch(err => console.error(err));
+    }else{
+        setCheck(true)
+    }
+    
 }
 
-const updateTodo=(todoId,text,setTodo,setText,setIsUpdating)=>{
+const updateTodo = (todoId, text, setTodo, setText, setIsUpdating,setCheck) => {
+    if (text) {
+        setCheck(false)
+        axios.post(`${Url}/update`, { _id: todoId, text })
+            .then(data => {
+                setText("")
+                setIsUpdating(false)
+                getAllTodo(setTodo)
+            })
+            .catch(err => console.error(err));
+    }else{
+        setCheck(true)
+    }
 
-    axios.post(`${Url}/update`,{_id:todoId,text})
-    .then(data=>{
-        setText("")
-        setIsUpdating(false)
-        getAllTodo(setTodo)
-    })
-    .catch(err=>console.error(err));
 }
 
-const deleteTodo=(_id,setTodo)=>{
+const deleteTodo = (_id, setTodo) => {
 
-    axios.post(`${Url}/delete`,{_id})
-    .then(data=>{        
-        getAllTodo(setTodo)
-    })
-    .catch(err=>console.error(err));
+    axios.post(`${Url}/delete`, { _id })
+        .then(data => {
+            getAllTodo(setTodo)
+        })
+        .catch(err => console.error(err));
 }
 
-export {getAllTodo,addTodo,updateTodo,deleteTodo}
+export { getAllTodo, addTodo, updateTodo, deleteTodo }
